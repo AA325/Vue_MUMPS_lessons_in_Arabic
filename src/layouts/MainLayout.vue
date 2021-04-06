33a30f17febf4b3b021375f54f35024ccea5f718
Ahmed <aa3255+github@gmail.com>
2021-04-06 04:44:49 -0700

    uploading the vuemumps framework

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          MUMPD - VUE
+        </q-toolbar-title>
+
+        <div>Demo v 0.0.1</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Links
+        </q-item-label>
+        <q-item clickable :to="'example1'">
+          <q-item-section>
+            <q-item-label>Example 1</q-item-label>
+            <q-item-label caption>
+              Communication between Vue and Mumps
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  }
+};
+</script>
---
+++ src/pages/Example1.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <q-input outlined v-model="input" label="Simple Input Box" />
+  <ul>
+      <li v-for="item in data" :key="item">
+          {{item}}
+      </li>    
+  </ul>
+     <q-btn color="secondary" label="Send to Server and Get Data" @click="sendDataToServer"/>
+  </div>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            data:[],
+            input:''
+        }
+    },
+    methods:{
+       async sendDataToServer(){
+        let data = await this.$M('EXAMPLE1^QDEMO',{
+            param: this.input
+        })
+       this.data = data.data
+       }
+    }
+}
+</script>
